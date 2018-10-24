31b7f9db305e6b7b0da8887239f57339ea0e8cef
dimomass <dimitar.dimitrov@aossia.com>
2018-10-24 13:34:58 +0300

    CRUD works properly

--- pinko-ldap/src/main/java/com/dimomass/pinko/PinkoLdapApplication.java
+++ pinko-ldap/src/main/java/com/dimomass/pinko/PinkoLdapApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PinkoApplication {
+public class PinkoLdapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PinkoApplication.class, args);
+		SpringApplication.run(PinkoLdapApplication.class, args);
 	}
 }
